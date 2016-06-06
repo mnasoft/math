39faeb8c8ec8c425d3dd5fe52bcee396bd884f13
Nick Matvyeyev <mnasoft@gmail.com>
2016-06-06 05:36:50 +0300

    Отлажена функция обратного хода

--- package.lisp
+++ package.lisp
@@ -1,8 +1,28 @@
 ;;;; package.lisp
 
-(defpackage #:math
+(defpackage #:math	       
   (:use #:cl #:cl-utilities)
-  (:export appr_table)
-  )
+  (:export appr_table   )
+  (:export matr-name
+	   matr-row
+	   matr-col
+	   ;; matr-elements
+	   matr-ij    
+	   matr-set_ij  
+	   matr-set-row 
+	   matr-get-row 
+	   matr-set-col 
+	   matr-get-col 
+	   matr-new     
+	   ;;  matr-eval
+	   matr-mult    
+	   matr-to-point
+	   point-to-matr
+	   matr-to-string
+	   matr-print
+	   matr-triang
+	   matr-las-gauss
+	   matr-las-rotation
+	   ))
 
 ;;;;(declaim (optimize (space 0) (compilation-speed 0)  (speed 0) (safety 3) (debug 3)))
