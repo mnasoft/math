bb3a035268b2e7b9e659c7afaa0a411b3d849411
Nick Matvyeyev <mnasoft@gmail.com>
2018-11-15 16:35:30 +0200

    2018-11-15 16:35:19 KO11-118383

--- matr-class.lisp
+++ matr-class.lisp
@@ -5,8 +5,10 @@
 (defclass matrix ()
   ((data :accessor matrix-data :initform nil :initarg :data)))
 
+(defmethod matr-name ((mm matrix)) "Matr")
+
 (defmethod print-object ((mm matrix) s)
-  (format s "~A " (matr-name))
+  (format s "~A " (matr-name mm))
   (when (and (matrix-data mm) (arrayp (matrix-data mm)))
     (format s "~{~A~^х~}" (array-dimensions (matrix-data mm)))
     (loop :for i :from 0 :below (array-dimension (matrix-data mm) 0)
@@ -14,12 +16,15 @@
 	 (format s "~%[")
 	 (loop :for j :from 0 :below (array-dimension (matrix-data mm) 1)
 	    :do (format s " ~8A " (aref (matrix-data mm) i j)))
-	 	 (format s "]"))))
+	 (format s "]"))))
+
+(defmethod  matr-print ((mm matrix) &optional (s t))
+  (print-object mm s))
 
 (defun matr-new (rows cols &optional (lst nil))
   "Примечание:
  (matr-new 3 4 '(1 2 3 4 5 6 7 8 9 10)) "
-  (let ((mm (make-instance 'matrix :data  (make-array (list rows cols) :initial-element 0.0d0)))
+  (let ((mm (make-instance 'matrix :data (make-array (list rows cols) :initial-element 0.0d0)))
 	(ll lst))
         (loop :for i :from 0 :below (array-dimension (matrix-data mm) 0)
        :do
@@ -28,79 +33,26 @@
 		      ll (cdr ll))))
 	mm))
 
-(defun matr-name ()
-  "Возвращает сторку \"Matr\"
-;;;;
-Пример использования:
-(matr-name)
-"
-  "Matr")
+(defmethod initialize-instance ((mm matrix) &key (rows 3) (cols 3) )
+  (setf (matrix-data mm) (make-array (list rows cols) :initial-element 0.0d0)))
 
-;;;;
-
-(defun matr-idx (i j m)
-  "Вычисляет индекс (i,j) элемента матрицы индексы начинаются с нуля
-m-количест во столбцов в матрице"
-  (+ (* i m) j))
-
-(defun matr-index (matr i j)
-  "Вычисляет индекс (i,j) элемента матрицы индексы начинаются с нуля
-m-количест во столбцов в матрице"
-  (matr-idx i j (matr-cols matr)))
-
-(defun matr-elements (matr)
-  "Возвращает список элементов матрицы
-Пример использования:
-(matr-elements '(\"Matr\" 2 3 ((0 . 0.0) (1 . 0.0) (2 . 0.0) (3 . 0.0) (4 . 0.0) (5 . 0.0))))
-;
-=>((0 . 0.0) (1 . 0.0) (2 . 0.0) (3 . 0.0) (4 . 0.0) (5 . 0.0))"
-  (cadddr matr))
-
-(defun matr-ind_err (i j n m)
-  "Пример использования:
-(matr-ind_err 0 0 2 3)
-"
-  (format nil "Ошибка при доступе к эл. матрицы: M[~A,~A] M[0..~A,0..~A]" i j n m))
-
-(defun in-range-inc (i i_min i_max ) (<= i_min i  i_max))
-
-(defun in-range-exc (i i_min i_max) (< i_min i  i_max))
-
-(defun in-range-inc-exc (i i_min i_max) (and (<= i_min i) (< i i_max)))
-
-(defun matr-index-is-good (matr i j)
-  (let ((n  (matr-rows matr))
-	(m  (matr-cols matr)))
-    (if (not (and(in-range-inc-exc i 0 n) (in-range-inc-exc j 0 m)))
-	(break "Ошибка при доступе к эл. матрицы: M[~A,~A] M[0..~A,0..~A]" i j (- n 1) (-  m 1 )))))
-
-(defun matr-set_ij (matr elem i j)
-  "Выполняет присваивание значения elem элементу матрицы matr(i j) элементы начинаются с нуля;
-Пример использования:
-(matr-set_ij 
- '(\"Matr\" 2 3 ((0 . 0.0) (1 . 0.0) (2 . 0.0) 
-		 (3 . 0.0) (4 . 0.0) (5 . 0.0)))
- 555.0 
- 1 1)
-"
-  (let ((n  (cadr matr))
-	(m  (caddr matr))
-	(li (cadddr matr)))
-    (matr-index-is-good matr i j)
-    (setf li (subst (cons (matr-idx i j m) elem)
-		    (assoc (matr-idx i j m) li)
-		    li))
-    (list (matr-name) n m li)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defmethod matr-rows-* ((mm matrix)) (array-dimension (matrix-data mm) 0))
-
-(defmethod matr-cols-* ((mm matrix)) (array-dimension (matrix-data mm) 1))
+(defmethod matr-copy ((mm-ref matrix))
+  (let* ((rows (matr-rows-* mm-ref))
+	 (cols	(matr-cols-* mm-ref) )
+	 (mm (make-instance 'matrix :data (make-array (list rows cols)))))
+    (loop :for i :from 0 :below rows
+       :do
+	 (loop :for j :from 0 :below cols
+	    :do (setf (aref (matrix-data mm) i j) (aref (matrix-data mm-ref) i j))))
+    mm))
 
 (defmethod matr-ij-*   ((mm matrix) i j) (aref (matrix-data mm) i j))
 
 (defmethod matr-set-ij-* ((mm matrix) value i j) (setf (aref (matrix-data mm) i j) value))
+
+(defmethod matr-rows-* ((mm matrix)) (array-dimension (matrix-data mm) 0))
+
+(defmethod matr-cols-* ((mm matrix)) (array-dimension (matrix-data mm) 1))
 
 (defmethod matr-set-row-* ((mm matrix) row pts)
   (let ((data (matrix-data mm))
@@ -114,56 +66,71 @@ m-количест во столбцов в матрице"
     (loop :for c :from 0 :below (matr-cols-* mm)
        :collect (aref data row c))))
 
-(matr-get-row-* *mm* 2)
+(defmethod matr-set-col-* ((mm matrix) col pts)
+    (let ((data (matrix-data mm))
+	(ll pts))
+    (loop :for r :from 0 :below (matr-rows-* mm)
+       :do (setf (aref data r col) (car ll)
+		 ll (cdr ll)))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-(defun matr-set-col(matr j pts)
-  "Пример использования;
-(matr-set-col
- '(\"Matr\" 2 3 ((0 . 1.0) (1 . 2.0) (2 . 3.0) 
-		 (3 . 4.0) (4 . 5.0) (5 . 6.0)))
- 0 '(11.0 12.0))"
-  (do ((n (matr-rows matr))
-       (i 0 (1+ i)))
-      ((>= i n) matr)
-    (setf matr (matr-set_ij matr (nth i pts)i j))))
-
-(defun matr-get-col (matr j)
-  "Пример использования:
-(matr-get-col
- '(\"Matr\" 2 3 ((0 . 1.0) (1 . 2.0) (2 . 3.0) 
-		 (3 . 4.0) (4 . 5.0) (5 . 6.0)))
- 1)
-"
-  (do ((n (matr-rows matr))
-       (i 0 (1+ i))
-       (pts nil))
-      ((>= i n)(reverse pts))
-    (setf pts (cons (matr-ij matr i j) pts))))
+(defmethod matr-get-col-* ((mm matrix) col)
+  (let ((data (matrix-data mm)))
+    (loop :for r :from 0 :below (matr-rows-* mm)
+       :collect (aref data r col))))
 
 
+
+(defmethod matr-eval ((mm matrix))
+  (let ((rows (matr-rows-* mm))
+	(cols (matr-cols-* mm))
+	(mm-cp (matr-copy  mm)))
+  (loop :for i :from 0 :below rows
+     :do
+       (loop :for j :from 0 :below cols
+	  :do (setf (aref (matrix-data mm) i j) (eval (aref (matrix-data mm) i j)))))
+  mm-cp))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun matr-eval (matr)
-  "Выполняет поэлементное оценивание каждого элемента матрицы
-Пример использования:
-(progn
-  (defparameter α (/ pi 3))
-  (let ((matr '(\"Matr\" 2 3
-		((0 . (sin α))  (1 . 0.0)     (2 . 0.0)
-		 (3 . 0.0)      (4 . (cos α)) (5 . 0.0)))))
-    (matr-eval matr)))
-"
-  (let ((n  (matr-rows matr))
-	(m  (matr-cols matr))
-	(li (matr-elements matr)))
-    (setf li (mapcar #'(lambda (el) (cons (car el) (eval (cdr el)))) li))
-    (list (matr-name) n m li)))
+(defclass x-o (matrix) ())
+
+(defparameter *xo* (make-instance 'x-o ))
+
+(defmethod matr-name ((mm x-o)) "X-O")
+
+(defmethod print-object ((mm x-o) s)
+  (format s "~A " (matr-name mm))
+  (when (and (matrix-data mm) (arrayp (matrix-data mm)))
+    (format s "~{~A~^х~}" (array-dimensions (matrix-data mm)))
+    (loop :for i :from 0 :below (array-dimension (matrix-data mm) 0)
+       :do
+	 (format s "~%[")
+	 (loop :for j :from 0 :below (array-dimension (matrix-data mm) 1)
+	    :do (format s " ~A " (aref (matrix-data mm) i j)))
+	 (format s "]"))))
+
+(defmethod initialize-instance ((mm x-o) &key (rows 3) (cols 3) )
+  (setf (matrix-data mm) (make-array (list rows cols) :initial-element 0)))  
+  
+(defparameter *mm* (matr-new 3 4) )
+(matr-set-col-* *mm* 0 '(1 2 3))
+(matr-get-row-* *mm* 2)
+
+(make-instance 'x-o)
+
+;;;;;
+
+(defmethod x-o-is-1-win ((mm x-o))
+  (loop :for r :from 0 :below (matr-rows-* mm)
+     :collect (matr-get-row-* mm r)))
+
+(or (mapcar #'(lambda (el) (member 1 (remove-duplicates el))) (x-o-is-1-win *xo*)))
+
+
+(defmethod x-o-is-2-win ((mm x-o))
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun matr-mult (a b)
   "Выполняет перемножение матриц a и b
@@ -243,46 +210,7 @@ m-количест во столбцов в матрице"
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun matr-copy (matr)
-  "Возвращает копию матрицы;
-Пример использования: 
-(matr-print 
- (matr-copy 
-  '(\"Matr\" 3 4 ((0 . 1.0)  (1 . 2.0) (2 . 3.0) (3 . 14.0) 
-		  (4 . 2.0)  (5 . 1.0) (6  . 1.0) (7  . 7.0) 
-		  (8 . 3.0)  (9 . 0.0) (10 . 1.0) (11 . 2.0)))))
-"
-  (matr-new (matr-rows matr) (matr-cols matr) (matr-to-point matr)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defun matr-to-string (matr)
-  "Выводит представление матрицы в виде строки символов
-;;;;
-Пример использования:
-(matr-to-string '( \"Matr\" 2 3
-	     ((0 . (sin *alfa*)) (1 . 0.0) (2 . 0.0)
-	      (3 . 0.0) (4 . (cos *alfa*)) (5 . 0.0))))
-"
-  (let ((n (matr-rows matr))
-	(m (matr-cols matr))
-	(out (make-string-output-stream)))
-    (format out "Matr ~S ~S~%" n m)
-    (do ((i 0 (1+ i))
-	 )
-	((>= i n) (get-output-stream-string out))
-      (format out "~S~%" (matr-get-row matr i)))))
-
-(defun matr-print (matr &optional (ostream t))
-  "Выводит представление матрицы в поток
-;;;;
-Пример использования:
-(matr-print '(\"Matr\" 2 3
-	     ((0 . (sin *alfa*)) (1 . 0.0) (2 . 0.0)
-	      (3 . 0.0) (4 . (cos *alfa*)) (5 . 0.0))))"
-  (format ostream "~A" (matr-to-string matr)))
-
-(defun matr-mnk(vv ff ex_pts)
+(defun matr-mnk (vv ff ex_pts)
   "Формирует точки для расчета коэффициентов по методу наименьших квадратов
 vv     - '(xx yy) - список, состоящий из имен факторов влияния
          и имени функции отклика;
