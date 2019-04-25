9b4bc38ce4d1c1602f1e56fb20af9e2d562e86e7
Nick Matvyeyev <mnasoft@gmail.com>
2019-04-25 16:41:33 +0300

    2019-4-25 16:41:21 KO11-118383

--- approximation.lisp
+++ approximation.lisp
@@ -48,16 +48,6 @@
 ;;;; Разработка
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(progn
-  (defparameter *test-data-01*
-    '((117.0 120.1 118.6 112.5 115.0)
-      (115.0 125.2 119.7 122.4 112.0)
-      (113.0 135.3 129.8 132.3 117.0)
-      (112.0 122.4 124.9 122.2 112.0)
-      (111.0 115.5 119.5 102.1 102.0)))
-  (defparameter *test-arr-data-01*
-    (make-array '(5 5) :initial-contents *test-data-01*)))
-
 (defparameter *apr-func-4*
   '((x1 x2) (x1) (x2) (1.0) (yy))
   "Шаблон для построения функции двух параметров: x1 и x2 c четырьмя коэффициентами.")
@@ -73,101 +63,22 @@
 "
   (eval (matr-osr-lambda '(x1 x2 yy) ff points)))
 
-(defun make-approximation-array-bilinear (a2d)
-  "Генерирует массив, содержащий в своих элементах функции двух переменных.
-Функции представляют билинейную интерполяцию массива a2d.
-Результирующий массив имеет размерности на единицу меньшую по всем направлениям (строкам и столбцам)."
-  (let ((a2-rez (make-array (mapcar #'1- (array-dimensions a2d)) :initial-element nil)))
-    (loop :for r :from 0 :below (array-dimension a2-rez 0) :do
-	 (loop :for c :from 0 :below (array-dimension a2-rez 1) :do
-	      (setf (aref a2-rez r c)
-		    (make-belinear-interpolation
-		     (list
-		      (list -1.0 -1.0 (aref a2d r c))
-		      (list -1.0  1.0 (aref a2d r (1+ c)))
-		      (list  1.0 -1.0  (aref a2d (1+ r) c))
-		      (list  1.0  1.0  (aref a2d (1+ r) (1+ c))))
-		     :ff *apr-func-4*))))
-    a2-rez))
-
-
-(defun test-make-approximation-array-bilinear (r c func-arr)
-  (make-array '(2 2)
-	      :initial-contents
-	      (list
-	       (list (funcall (aref func-arr r c) -1 -1)
-		     (funcall (aref func-arr r c) -1  1))
-	       (list (funcall (aref func-arr r c)  1 -1)
-		     (funcall (aref func-arr r c)  1  1)))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; Тестирование
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-(defparameter *asd* (make-approximation-array-bilinear *test-arr-data-01*))
-
-
-(defun foo-cols (x x-num-func)
-  (let ((col (round (+ (- x  1/2)(/ x-num-func 2)))))
-    (cond
-      ((< col 0) 0)
-      ((>= col x-num-func) 3)
-      (t col))))
-
-(defun foo-x (x x-num-func)
-  (let* ((col (+ (- x  1/2)(/ x-num-func 2))))
-    (* 2 (- col (foo-cols x x-num-func) ))))
-
-(defun foo-rows (y y-num-func)
-  (let ((row (round (+ (- (* y -1)  1/2)(/ y-num-func 2)))))
-    (cond
-      ((< row 0) 0)
-      ((>= row y-num-func) 3)
-      (t row))))
-
-(defun foo-y (y y-num-func)
-  (let* ((row (+ (- (* y -1) 1/2) (/ y-num-func 2))))
-    (* 2 (- row (foo-rows y y-num-func)))))
-
-(defun foo (x y x-num-func y-num-func)
-  (list
-   (list (foo-x x x-num-func) (foo-y y y-num-func))
-   (list (foo-rows y y-num-func)
-	 (foo-cols x x-num-func))))
-
-(defun bar (x y a2d-func )
-  (let* ((x-num-func (array-dimension a2d-func 1))
-	 (y-num-func (array-dimension a2d-func 0))
-	 (n (foo x y x-num-func y-num-func))
-	 (x1-x2 (first n))
-	 (x1    (first  x1-x2))
- 	 (x2    (second x1-x2))
-	 (rc    (second n))
-	 (r     (first  rc))
- 	 (c     (second rc)))
-    (values (funcall (aref a2d-func r c) x2 x1 ) "x=" x "y=" y "rc=" rc "x1-x2" x1-x2 (test-make-approximation-array-bilinear r c a2d-func) *test-arr-data-01*)))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defun make-approximation-array-bilinear-01 (a2d x1 x2)
+(defun make-approximation-array-bilinear (a2d x1 x2)
   "Генерирует массив, содержащий в своих элементах функции двух переменных.
 Функции представляют билинейную интерполяцию массива a2d.
 Результирующий массив имеет размерности на единицу меньшую по всем направлениям (строкам и столбцам).
-|------+---+---------+---------+---------+---------+---------|
-|      |   | x2-0    | x2-1    | x2-2    | x2-3    | x2-4    |
-|------+---+---------+---------+---------+---------+---------|
-|------+---+---------+---------+---------+---------+---------|
-| x1-0 |   | a2d-0,0 | a2d-0,1 | a2d-0,2 | a2d-0,3 | a2d-0,4 |
-| x1-1 |   | a2d-1,0 | a2d-1,1 | a2d-1,2 | a2d-1,3 | a2d-1,4 |
-| x1-2 |   | a2d-2,0 | a2d-2,1 | a2d-2,2 | a2d-2,3 | a2d-2,4 |
-| x1-3 |   | a2d-3,0 | a2d-3,1 | a2d-3,2 | a2d-3,3 | a2d-3,4 |
-| x1-4 |   | a2d-4,0 | a2d-4,1 | a2d-4,2 | a2d-4,3 | a2d-4,4 |
+|-------+---+----------+----------+----------+----------+----------|
+|       |   | x2[0]    | x2[1]    | x2[2]    | x2[3]    | x2[4]    |
+|-------+---+----------+----------+----------+----------+----------|
+|-------+---+----------+----------+----------+----------+----------|
+| x1[0] |   | a2d[0,0] | a2d[0,1] | a2d[0,2] | a2d[0,3] | a2d[0,4] |
+| x1[1] |   | a2d[1,0] | a2d[1,1] | a2d[1,2] | a2d[1,3] | a2d[1,4] |
+| x1[2] |   | a2d[2,0] | a2d[2,1] | a2d[2,2] | a2d[2,3] | a2d[2,4] |
+| x1[3] |   | a2d[3,0] | a2d[3,1] | a2d[3,2] | a2d[3,3] | a2d[3,4] |
+| x1[4] |   | a2d[4,0] | a2d[4,1] | a2d[4,2] | a2d[4,3] | a2d[4,4] |
 
 "
-  (when (/= 2 (array-rank a2d)) (error "In make-approximation-array-bilinear-01: (/= 2 (array-rank a2d))"))
+  (when (/= 2 (array-rank a2d)) (error "In make-approximation-array-bilinear: (/= 2 (array-rank a2d))"))
   (let ((a2-rez (make-array (mapcar #'1- (array-dimensions a2d)) :initial-element nil)))
     (loop :for r :from 0 :below (array-dimension a2-rez 0) :do
 	 (loop :for c :from 0 :below (array-dimension a2-rez 1) :do
@@ -181,33 +92,50 @@
 		     :ff *apr-func-4*))))
     a2-rez))
 
-;;(make-approximation-array-bilinear-01  *test-arr-data-01*  (vector -2.0 -1.0 0.0 1.0 2.0)  (vector -2.0 -1.0 0.0 1.0 2.0))
+(defun approximation-bilinear (v1 v2 a-x1 a-x2 a-func)
+  (labels ((index-by-value (val vect)
+	     (let ((rez+ 0)
+		   (rez- (- (array-dimension vect 0) 2)))
+	       (cond
+		 ((< (svref vect 0) (svref vect (1- (array-dimension vect 0))))
+		  (loop :for i :from 1 :below (1- (array-dimension vect 0)) :do
+		       (when (<= (svref vect i) val ) (setf rez+ i)))
+		  rez+)
+		 ((> (svref vect 0) (svref vect (1- (array-dimension vect 0))))
+		  (loop :for i :from (1- (array-dimension vect 0)) :downto 1 :do
+		       (when (>= val (svref vect i)) (setf rez- (1- i))))
+		  rez-)))))
+    (funcall (aref a-func (index-by-value v1 a-x1) (index-by-value v2 a-x2)) v1 v2)))
 
-(svref (vector -2.0 -1.0 0.0 1.0 2.0) 1)
-(elt '(list 1 2 3 4) 1)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Тестирование
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun index-by-value (val vect)
-  (let ((rez 0))
-    (loop :for i :from 1 :below (1- (array-dimension vect 0)) :do
-	 (when (<= (svref vect i) val ) (setf rez i)))
-    rez))
+(progn
+  (defparameter *test-data-01*
+    '((117.0 120.1 118.6 112.5 115.0)
+      (115.0 125.2 119.7 122.4 112.0)
+      (113.0 135.3 129.8 132.3 117.0)
+      (112.0 122.4 124.9 122.2 112.0)
+      (111.0 115.5 119.5 102.1 102.0)))
+  (defparameter *test-arr-data-01*
+    (make-array '(5 5) :initial-contents *test-data-01*)))
 
-(defparameter
-    *asd-01* ( make-approximation-array-bilinear-01
-	      *test-arr-data-01* 
-	      (vector -2.0 -1.0 0.0 1.0 2.0)
-	      (vector -2.0 -1.0 0.0 1.0 2.0)))
+(defparameter *asd-01* (make-approximation-array-bilinear
+			*test-arr-data-01* 
+			(vector  2.0  1.0 0.0 -1.0 -2.0)
+			(vector -2.0 -1.0  0.0 1.0 2.0)))
 
-(defun bar-01 (v1 v2 a-x1 a-x2 a-func)
-  (funcall (aref a-func (index-by-value v1 a-x1) (index-by-value v2 a-x2)) v1 v2))
+(approximation-bilinear 2.5 -0.001
+	(vector 2.0 1.0 0.0 -1.0 -2.0)
+	(vector -2.0 -1.0 0.0 1.0 2.0)
+	*asd-01*)
 
-#2A((117.0 120.1 118.6 112.5 115.0)
-    (115.0 125.2 119.7 122.4 112.0)
-    (113.0 135.3 129.8 132.3 117.0)
-    (112.0 122.4 124.9 122.2 112.0)
-    (111.0 115.5 119.5 102.1 102.0))
+(loop :for h :from -25 :to 25 :collect
+     (loop :for x :from -25 :to 25 :collect
+	  (approximation-bilinear (/ h 10.0) (/ x 10.0)
+				  (vector 2.0 1.0 0.0 -1.0 -2.0)
+				  (vector -2.0 -1.0 0.0 1.0 2.0)
+				  *asd-01*)))
 
-(bar-01 2.0 1.50 (vector -2.0 -1.0 0.0 1.0 2.0)  (vector -2.0 -1.0 0.0 1.0 2.0) *asd-01*)
-
-
-
+(defun gnuplot-2dlist (2dlist x1-lst x2-lst))
