1a12753dadc05d8c018aef6034d10aa20a06d37a
Nick Matvyeyev <mnasoft@gmail.com>
2019-05-13 14:08:39 +0300

    2019-5-13 14:8:24 MNASOFT-10

--- matr.lisp
+++ matr.lisp
@@ -469,24 +469,7 @@ Matr 3 4
     (setf rez (list 'defun func-name (reverse (cdr(reverse vv))) kk))
     rez))
 
-(export 'matr-osr-lambda)
-(defun matr-osr-lambda (vv ff ex_pts)
-  "Пример использования:
-  (matr-osr-func '(xx yy) 
-		 '((xx xx) (xx) (1.0) (yy)) 
-		 '((-1.0 1.0) (0.0 0.0) (2.0 4.0) (3.0 9.0))
-		 'coool-func)
-;
-=>(\"Matr\" 1 3 ((0 . 1.0d0) (1 . 0.0d0) (2 . 0.0d0)))
-"
-  (let ((kk (cons '+ (mapcar #'(lambda(el1 el2) (cons '* (cons el1 el2)))
-			     (matr-to-point 
-			      (matr-las-gauss
-			       (matr-mnk vv ff ex_pts)))
-			     ff)))
-	(rez nil))
-    (setf rez (list 'lambda (reverse (cdr(reverse vv))) kk))
-    rez))
+
 
  
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
