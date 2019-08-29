6206a57e81e0a2d647d73f082c14884368180eb4
Nick Matvyeyev <mnasoft@gmail.com>
2019-08-29 14:03:18 +0300

    2019-8-29 14:3:8 MNASOFT-01

--- package.lisp
+++ package.lisp
@@ -126,3 +126,5 @@
   )
 
 ;;;;(declaim (optimize (space 0) (compilation-speed 0)  (speed 0) (safety 3) (debug 3)))
+
+;;;; (declaim (optimize (compilation-speed 0) (debug 3) (safety 0) (space 0) (speed 0)))
