f61381716dbe39eb85878869f644449e3d3d9996
Nick Matvyeyev <mnasoft@gmail.com>
2020-05-24 23:47:58 +0300

    Вс 24 мая 2020 20:47:58 UTC

--- matr-class.lisp
+++ matr-class.lisp
@@ -196,6 +196,39 @@
   mm)
 
 @export
+(defmethod (setf main-diagonal) ((element number) (mm <matrix>) )
+  "@b(Пример использования:)
+@begin[lang=lisp](code)
+ (defparameter *mm* 
+   (make-instance '<matrix> 
+      :initial-contents '(( 1d0  2d0  3d0) 
+                          ( 4d0  5d0  6d0) 
+                          ( 7d0  8d0  9d0) 
+                          (10d0 11d0 12d0))))
+ *mm* =>  Matr 4х3
+          [ 1.0d0     2.0d0     3.0d0    ]
+          [ 4.0d0     5.0d0     6.0d0    ]
+          [ 7.0d0     8.0d0     9.0d0    ]
+          [ 10.0d0    11.0d0    12.0d0   ]
+
+ (setf (main-diagonal *mm*) 11d0) 
+  Matr 4х3
+  [ 11.0d0     2.0d0    3.0d0   ]
+  [ 4.0d0     11.0d0    6.0d0   ]
+  [ 7.0d0      8.0d0   11.0d0   ]
+  [ 10.0d0    11.0d0   12.0d0   ]
+
+ *mm* => Matr 4х3
+  [ 11.0d0    2.0d0     3.0d0    ]
+  [ 4.0d0     11.0d0    6.0d0    ]
+  [ 7.0d0     8.0d0     11.0d0   ]
+  [ 10.0d0    11.0d0    12.0d0   ]
+@end(code)"  
+  (loop :for i :from 0 :below (min (rows mm) (cols mm))
+	:do (setf (mref  mm i i) element))
+  mm)
+
+@export
 (defmethod  squarep ((mm <matrix>))
   "@b(Пример использования:)
 @begin[lang=lisp](code)
