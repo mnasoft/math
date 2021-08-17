e9f146aa067bfc122f3cce540cb1117bf5f9bcbc
Nick Matvyeyev <mnasoft@gmail.com>
2021-08-17 10:37:58 +0300

    17 авг 2021 г. 10:37:58

--- src/arr-matr/matr-generics.lisp
+++
@@ -1,9 +0,0 @@
-;;;; matr-generics.lisp
-
-(in-package :math/arr-matr)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;;;; (setf *print-case* :downcase)
-;;;; (setf *print-case* :upcase)
--- src/arr-matr/package.lisp
+++
@@ -1,2 +0,0 @@
-;;;; /src/arr-matr/package.lisp
-
