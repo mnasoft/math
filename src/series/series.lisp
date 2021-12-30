5f075829081f49edc75e57c5c60f6330bf00fc94
Nick Matvyeyev <mnasoft@gmail.com>
2021-12-30 13:47:24 +0200

    30 дек 2021 г. 13:47:24

--- src/series/series.lisp
+++ src/series/series.lisp
@@ -14,28 +14,77 @@
   (:documentation "Квадратное уравнение."))
 
 (defclass cubic ()
-  ((a :accessor quadric-a :initform 1.0 :initarg :a :documentation "Коэффициент при степени 3.")
-   (b :accessor quadric-b :initform 0.0 :initarg :b :documentation "Коэффициент при степени 2.")
-   (c :accessor quadric-c :initform 1.0 :initarg :c :documentation "Коэффициент при степени 1.")
-   (d :accessor quadric-c :initform 1.0 :initarg :c :documentation "Коэффициент при степени 0."))
+  ((a :accessor quadric-a :initform  1.0 :initarg :a :documentation "Коэффициент при степени 3.")
+   (b :accessor quadric-b :initform -1.0 :initarg :b :documentation "Коэффициент при степени 2.")
+   (c :accessor quadric-c :initform  0.0 :initarg :c :documentation "Коэффициент при степени 1.")
+   (d :accessor quadric-d :initform -1.0 :initarg :d :documentation "Коэффициент при степени 0."))
   (:documentation "Кубическое уравнение."))
 
 (defclass quartic ()
   ((a :accessor quadric-a :initform 1.0 :initarg :a :documentation "Коэффициент при степени 4.")
    (b :accessor quadric-b :initform 0.0 :initarg :b :documentation "Коэффициент при степени 3.")
-   (c :accessor quadric-c :initform 1.0 :initarg :c :documentation "Коэффициент при степени 2.")
-   (d :accessor quadric-c :initform 1.0 :initarg :c :documentation "Коэффициент при степени 1.")
-   (e :accessor quadric-c :initform 1.0 :initarg :c :documentation "Коэффициент при степени 0."))
+   (c :accessor quadric-c :initform 0.0 :initarg :c :documentation "Коэффициент при степени 2.")
+   (d :accessor quadric-d :initform 0.0 :initarg :c :documentation "Коэффициент при степени 1.")
+   (e :accessor quadric-e :initform 1.0 :initarg :c :documentation "Коэффициент при степени 0."))
   (:documentation "Уравнение четвертой степени."))
 
-(defgeneric roots ((eq quartic))
-  
+(defgeneric tab (eq x)
   )
 
-(defgeneric roots ((eq cubic))
-  
+(defmethod tab ((eq quadric) x)
+  (let ((a (quadric-a eq))
+        (b (quadric-b eq))
+        (c (quadric-c eq)))
+    (+ (* a x x) (* b x) c)))
+
+(defmethod tab ((eq cubic) x)
+  (let ((a (quadric-a eq))
+        (b (quadric-b eq))
+        (c (quadric-c eq))
+        (d (quadric-d eq)))
+    (+ (* a x x x) (* b x x) (* c x) d)))
+
+
+*a*
+
+(defgeneric roots (eq)
   )
-  
+
+(loop :for x :from -10.0 :to 10.0 :do 
+  (format t "~a ~a~%" x (tab (make-instance 'cubic) x)))
+
+(tab (make-instance 'cubic) (second (roots (make-instance 'cubic))))
+
+(defmethod roots ((eq cubic))
+  (let ((a (quadric-a eq))
+        (b (quadric-b eq))
+        (c (quadric-c eq))
+        (d (quadric-d eq)))
+    (let* ((d0 (- (* b b) (* 3d0 a c)))
+           (d1 (+ (* 2d0 b b b) (* -9d0 a b c) (* 27d0 a a d)))
+           (c+ (expt (/ (+ d1 (sqrt (- (* d1 d1)(* 4d0 d0 d0 d0)))) 2d0) (/ 3d0)))
+           (c- (expt (/ (- d1 (sqrt (- (* d1 d1)(* 4d0 d0 d0 d0)))) 2d0) (/ 3d0)))
+           (ξ (* 0.5d0 (+ -1d0 (sqrt -3d0)))))
+      (list 
+       (* (/ -1 (* 3 a))
+          (+ b (* (expt ξ 0) c+) (/ d0 (* (expt ξ 0) c+))
+             ))
+       (* (/ -1 (* 3 a))
+          (+ b (* (expt ξ 0) c-) (/ d0 (* (expt ξ 0) c-))))
+
+       (* (/ -1 (* 3 a))
+          (+ b (* (expt ξ 1) c+) (/ d0 (* (expt ξ 1) c+))
+             ))
+       (* (/ -1 (* 3 a))
+          (+ b (* (expt ξ 1) c-) (/ d0 (* (expt ξ 1) c-))))
+
+       (* (/ -1 (* 3 a))
+          (+ b (* (expt ξ 2) c+) (/ d0 (* (expt ξ 2) c+))
+             ))
+       (* (/ -1 (* 3 a))
+          (+ b (* (expt ξ 2) c-) (/ d0 (* (expt ξ 2) c-))))
+       ))))
+
 (defmethod roots ((eq quadric))
   (let ((a (quadric-a eq))
         (b (quadric-b eq))
