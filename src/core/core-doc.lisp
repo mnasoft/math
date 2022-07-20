3c27129176ebe9474e4a5d2163ef763fce11df89
Mykola Matvyeyev <mnasoft@gmail.com>
2022-07-20 17:32:12 +0300

    Ср 20 июл 2022 14:32:12 UTC

--- src/core/core-doc.lisp
+++ src/core/core-doc.lisp
@@ -1,5 +1,15 @@
 (in-package :math/core)
 
+(defmacro make-doc (obj-name obj-type doc-string)
+  `(setf (documentation ,obj-name ,obj-type)
+         ,doc-string))
+
+(defun find-slot (slot-name class)
+  (find slot-name
+        (sb-mop:class-direct-slots  (find-class class))
+        :key #'sb-mop:slot-definition-name))
+
+
 (make-doc
  #'square
  'function 
