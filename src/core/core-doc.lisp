ed630b6f84ac5a69b429ca93ddcefa2c52774648
Mykola Matvyeyev <mnasoft@gmail.com>
2022-07-21 09:09:23 +0300

    Чт 21 июл 2022 06:09:23 UTC

--- src/core/core-doc.lisp
+++ src/core/core-doc.lisp
@@ -10,10 +10,14 @@
         :key #'sb-mop:slot-definition-name))
 
 
+
 (make-doc
- #'square
- 'function 
- "@b(Описание:) функция @b(square) возвращает квадрат значения.
+  'MATH/CORE:+SIGNIFICANT-DIGITS+ 'variable
+  "Определяет количество значащих цифр при округлении по умолчанию.")
+
+(make-doc
+  #'MATH/CORE:SQUARE 'function
+  "@b(Описание:) функция @b(square) возвращает квадрат значения.
 
  @b(Переменые:)
 @begin(list)
@@ -27,9 +31,26 @@
 @end(code)")
 
 (make-doc
- #'exclude-nil-from-list
- 'function 
- "@b(Описание:) функция @b(exclude-nil-from-list) возвращает список в
+  #'MATH/CORE:SPLIT-RANGE-BY-FUNC 'function
+  "@b(Описание:) split-range-by-func
+
+ @b(Пример использования:)
+@begin[lang=lisp](code)
+ (split-range-by-func 1 10 5) => (1.0 1.5848932 2.5118864 3.981072 6.3095737 10.0)
+ (split-range-by-func 1 10 10) =>
+ (1.0 1.2589254 1.5848932 1.9952624 2.5118864 3.1622777 3.981072 5.0118723  6.3095737 7.943282 10.0)
+@end(code)
+")
+
+(make-doc
+  #'MATH/CORE:DEPTH-SPHERE-ALONG-CONE 'function
+  "@b(Описание:) функция @b(depth-sphere-along-cone) возвращает 
+заглубление сферы с радиусом R в конуc с углом при вершине 
+равным alpha от линии пересечения конуса с цилиндром.")
+
+(make-doc
+  #'MATH/CORE:EXCLUDE-NIL-FROM-LIST 'function
+  "@b(Описание:) функция @b(exclude-nil-from-list) возвращает список в
  котором нет nil-элементов (они исключаются).
 
  @b(Пример использования:) @begin[lang=lisp](code)
@@ -38,63 +59,18 @@
 @end(code)")
 
 (make-doc
- #'e-value
- 'function
- "Возвращает приближенное значение числа e
-Пример использования:
-  (coerce (e-value      1) 'double-float) 2.0d0
-  (coerce (e-value      2) 'double-float) 2.5d0
-  (coerce (e-value      4) 'double-float) 2.7083333333333335d0
-  (coerce (e-value      6) 'double-float) 2.7180555555555554d0
-  (coerce (e-value      8) 'double-float) 2.71827876984127d0
-  (coerce (e-value     10) 'double-float) 2.7182818011463845d0
-  (coerce (e-value     15) 'double-float) 2.7182818284589945d0
-  (coerce (e-value     16) 'double-float) 2.7182818284590424d0
-  (coerce (e-value     17) 'double-float) 2.718281828459045d0
-  (coerce (e-value  10000) 'double-float) 2.718281828459045d0
-  (exp 1.0d0)                             2.718281828459045d0
-"   
-
- )
-
-(make-doc
- #'split-range
- 'function
- "@b(Описание:) split-range
+  #'MATH/CORE:SPLIT-RANGE 'function
+  "@b(Описание:) split-range
 
  @b(Пример использования:)
 @begin[lang=lisp](code)
  (split-range 10 20 5)  => (10.0 12.0 14.0 16.0 18.0 20.0)
 @end(code)
- "
- )
+ ")
 
 (make-doc
- #'split-range-by-func
- 'function
- "@b(Описание:) split-range-by-func
-
- @b(Пример использования:)
-@begin[lang=lisp](code)
- (split-range-by-func 1 10 5) => (1.0 1.5848932 2.5118864 3.981072 6.3095737 10.0)
- (split-range-by-func 1 10 10) =>
- (1.0 1.2589254 1.5848932 1.9952624 2.5118864 3.1622777 3.981072 5.0118723  6.3095737 7.943282 10.0)
-@end(code)
-"
- )
-
-(make-doc
- #'depth-sphere-along-cone
- 'function
- "@b(Описание:) функция @b(depth-sphere-along-cone) возвращает 
-заглубление сферы с радиусом R в конуc с углом при вершине 
-равным alpha от линии пересечения конуса с цилиндром."
- )
-
-(make-doc
- #'round-to-significant-digits
- 'function
- "@b(Описание:) функция @b(round-to-significant-digits) округляет значение
+  #'MATH/CORE:ROUND-TO-SIGNIFICANT-DIGITS 'function
+  "@b(Описание:) функция @b(round-to-significant-digits) округляет значение
 val до количества значащих цифр, задаваемых аргументом significant-digits.
 
  @b(Пример использования:)
@@ -103,83 +79,23 @@ val до количества значащих цифр, задаваемых а
  (round-to-significant-digits 456.32738915923 6         ) => 456.327
  (round-to-significant-digits 456.32738915923 6 53562.23) => 456.3
 @end(code)
-"
- )
-
-(make-doc '+significant-digits+ 'variable
-          "Определяет количество значащих цифр при округлении по умолчанию.")
+")
 
 (make-doc
- #'summ-distance 'function
- "@b(Описание:) обобщенная функция @b(summ-distance) 
-возвращает сумму расстояний по каждому направлению."
- )
-
-(make-doc
- #'summ-distance 'function
- "@b(Описание:) обобщенная функция @b(summ-distance) 
-возвращает сумму расстояний по каждому направлению."
- )
-
-(make-doc
- #'distance 'function
- "@b(Описание:) обобщенная функция @b(distance)
-возвращает расстояние между x1 и x2. Как корень квадратный из 
-сумм квадратов расстояний по каждому направлению.")
-
-(make-doc
- #'distance-relative 'function
- "@b(Описание:) обобщенная функция @b(distance-relative)
+  #'MATH/CORE:DISTANCE-RELATIVE 'function
+  "@b(Описание:) обобщенная функция @b(distance-relative)
 возвращает относительную длину между x и xi, длина приведения dx.
 Корень квадратный из сумм квадратов расстояний по каждому направлению
 отнесенному к длине приведения.")
 
 (make-doc
- #'matr-name-* 'function
- "Matr")
+  #'MATH/CORE:TRANSPOSE 'function
+  "@b(Описание:) обобщенная_функция @b(transpose) 
+возвращает транспонированную матрицу.")
 
 (make-doc
- #'dimensions 'function 
- "@b(Описание:) обобщенная_функция @b(dimensions) возвращает список,
-содержащий размерности матрицы @b(matrix).")
-
-(make-doc
- #'rows 'function
- "@b(Описание:) обобщенная_функция @b(rows) возврвщает количество строк
-матрицы @b(matrix).")
-
-(make-doc
- #'cols 'function
- "@b(Описание:) обобщенная_функция @b(cols) возврвщает количество столбцов
-матрицы @b(matrix).")
-
-(make-doc
- #'equivalent 'function
- "@b(Описание:) обобщенная_функция @b(equivalent) возвращает T,
-если матирицы @b(matrix-1) и @b(matrix-2) имеют одинаковые размерности и их 
-соответствующие элементы равны (для них функция @b(test) возвращает T ).")
-
-(make-doc
- #'main-diagonal 'function
- "@b(Описание:) обобщенная_функция @b(main-diagonal) извлекает главную
- диагональ матрицы.
-
-Элементы возвращаются в порядке возрастания строк.")
-
-(make-doc
- #'row 'function
- "@b(Описание:) обобщенная_функция @b(row) 
-возвращает строку @b(row) матрицы @b(matrix).")
-
-(make-doc
- #'col 'function
- "@b(Описание:) обобщенная_функция @b(col) 
-возвращает строку @b(col) матрицы @b(matrix).")
-
-
-(make-doc
- #'anti-diagonal 'function
- "@b(Описание:) обобщенная_функция @b(anti-diagonal)
+  #'MATH/CORE:ANTI-DIAGONAL 'function
+  "@b(Описание:) обобщенная_функция @b(anti-diagonal)
 возвращает список элементов, находящихся на побочной диагонали матрицы.
 
  В результирующем списке элементы следуют по строкам.
@@ -187,40 +103,85 @@ val до количества значащих цифр, задаваемых а
  Д.б опредена только для квадратной матрицы.")
 
 (make-doc
- #'squarep 'function
- "@b(Описание:) обобщенная_функция @b(squarep) возвращает T, 
-если матрица @b(matrix) является квадратной.")
+  #'MATH/CORE:ROW 'function
+  "@b(Описание:) обобщенная_функция @b(row) 
+возвращает строку @b(row) матрицы @b(matrix).")
 
 (make-doc
- #'mref 'function
- "@b(Описание:) обобщенная_функция @b(mref) возвращает элемент матрицы,
+  #'MATH/CORE:SUMM-DISTANCE 'function
+  "@b(Описание:) обобщенная функция @b(summ-distance) 
+возвращает сумму расстояний по каждому направлению.")
+
+(make-doc
+  #'MATH/CORE:MREF 'function
+  "@b(Описание:) обобщенная_функция @b(mref) возвращает элемент матрицы,
 находяшийся в строке @b(row) и столбце @b(col).")
 
 (make-doc
- #'copy 'function
- "@b(Описание:) обобщенная_функция @b(copy) возвращает ссылку на новый объект,
+  #'MATH/CORE:ROWS 'function
+  "@b(Описание:) обобщенная_функция @b(rows) возврвщает количество строк
+матрицы @b(matrix).")
+
+(make-doc
+  #'MATH/CORE:COPY 'function
+  "@b(Описание:) обобщенная_функция @b(copy) возвращает ссылку на новый объект,
 созданный на основе @b(obj).")
 
 (make-doc
- #'add 'function
- "@b(Описание:) обобщенная_функция @b(multiply)
+  #'MATH/CORE:MAIN-DIAGONAL 'function
+  "@b(Описание:) обобщенная_функция @b(main-diagonal) извлекает главную
+ диагональ матрицы.
+
+Элементы возвращаются в порядке возрастания строк.")
+
+(make-doc
+  #'MATH/CORE:EQUIVALENT 'function
+  "@b(Описание:) обобщенная_функция @b(equivalent) возвращает T,
+если матирицы @b(matrix-1) и @b(matrix-2) имеют одинаковые размерности и их 
+соответствующие элементы равны (для них функция @b(test) возвращает T ).")
+
+(make-doc
+  #'MATH/CORE:DIMENSIONS 'function
+  "@b(Описание:) обобщенная_функция @b(dimensions) возвращает список,
+содержащий размерности матрицы @b(matrix).")
+
+(make-doc
+  #'MATH/CORE:COL 'function
+  "@b(Описание:) обобщенная_функция @b(col) 
+возвращает строку @b(col) матрицы @b(matrix).")
+
+(make-doc
+  #'MATH/CORE:ADD 'function
+  "@b(Описание:) обобщенная_функция @b(multiply)
 выполняет сложение аргументов @b(a) и @b(b).")
 
 (make-doc
- #'multiply 'function
- "@b(Описание:) обобщенная_функция @b(multiply) 
+  #'MATH/CORE:MULTIPLY 'function
+  "@b(Описание:) обобщенная_функция @b(multiply) 
 выполняет перемножение аргументов @b(a) и @b(b).")
 
 (make-doc
- #'transpose 'function
- "@b(Описание:) обобщенная_функция @b(transpose) 
-возвращает транспонированную матрицу.")
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+  #'MATH/CORE:SQUAREP 'function
+  "@b(Описание:) обобщенная_функция @b(squarep) возвращает T, 
+если матрица @b(matrix) является квадратной.")
 
 (make-doc
-  (find-method #'DISTANCE NIL '(NUMBER NUMBER))
+  #'MATH/CORE:COLS 'function
+  "@b(Описание:) обобщенная_функция @b(cols) возврвщает количество столбцов
+матрицы @b(matrix).")
+
+(make-doc
+  #'MATH/CORE:MATR-NAME-* 'function
+  "Matr")
+
+(make-doc
+  #'MATH/CORE:DISTANCE 'function
+  "@b(Описание:) обобщенная функция @b(distance)
+возвращает расстояние между x1 и x2. Как корень квадратный из 
+сумм квадратов расстояний по каждому направлению.")
+
+(make-doc
+  (find-method #'MATH/CORE:DISTANCE NIL '(NUMBER NUMBER))
   t
   "@b(Описание:) 
 
@@ -231,7 +192,7 @@ val до количества значащих цифр, задаваемых а
 ")
 
 (make-doc
-  (find-method #'DISTANCE NIL '(CONS CONS))
+  (find-method #'MATH/CORE:DISTANCE NIL '(CONS CONS))
   t
   "@b(Описание:) метод @b(distance) возвращает расстояние 
 между точками @b(x1-lst) и @b(x2-lst).
@@ -245,7 +206,7 @@ val до количества значащих цифр, задаваемых а
 ")
 
 (make-doc
-  (find-method #'DISTANCE NIL '(VECTOR VECTOR))
+  (find-method #'MATH/CORE:DISTANCE NIL '(VECTOR VECTOR))
   t
   "@b(Описание:) метод @b(distance) возвращает расстояние 
 между точками @b(x1-lst) и @b(x2-lst).
@@ -259,7 +220,7 @@ val до количества значащих цифр, задаваемых а
 ")
 
 (make-doc
-  (find-method #'SUMM-DISTANCE NIL '(VECTOR VECTOR))
+  (find-method #'MATH/CORE:SUMM-DISTANCE NIL '(VECTOR VECTOR))
   t
   "@b(Описание:) функция @b(summ-distance) возвращает сумму 
 расстояний по каждому направлению.
@@ -271,7 +232,7 @@ val до количества значащих цифр, задаваемых а
 ")
 
 (make-doc
-  (find-method #'SUMM-DISTANCE NIL '(CONS CONS))
+  (find-method #'MATH/CORE:SUMM-DISTANCE NIL '(CONS CONS))
   t
   "@b(Описание:) функция @b(summ-distance) возвращает сумму 
 расстояний по каждому направлению.
@@ -283,7 +244,7 @@ val до количества значащих цифр, задаваемых а
 ")
 
 (make-doc
-  (find-method #'DISTANCE-RELATIVE NIL '(NUMBER NUMBER NUMBER))
+  (find-method #'MATH/CORE:DISTANCE-RELATIVE NIL '(NUMBER NUMBER NUMBER))
   t
   "Пример использования:
  (distance-relative 1 0 1) 1.0
@@ -292,7 +253,7 @@ val до количества значащих цифр, задаваемых а
 ")
 
 (make-doc
-  (find-method #'DISTANCE-RELATIVE NIL '(CONS CONS CONS))
+  (find-method #'MATH/CORE:DISTANCE-RELATIVE NIL '(CONS CONS CONS))
   t
   "Пример использования:
  (distance-relative '(1 1 1) '(0 0 0) '(1 1 1)) 1.7320508
@@ -301,7 +262,7 @@ val до количества значащих цифр, задаваемых а
 ")
 
 (make-doc
-  (find-method #'DISTANCE-RELATIVE NIL '(VECTOR VECTOR VECTOR))
+  (find-method #'MATH/CORE:DISTANCE-RELATIVE NIL '(VECTOR VECTOR VECTOR))
   t
   "@b(Описание:) метод @b(distance-relative) возвращает относительное 
 расстояние от точки @b(x) до точки @b(xi) по отношению к базовым длинам,
