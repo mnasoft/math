ea3c3fed4913ce8bff86e9a4bacdbe9e2ea7e6d4
Mykola Matvyeyev <mnasoft@gmail.com>
2026-04-02 23:12:35 +0300

    Улучшена документация math/smooth

--- src/smooth/smooth.lisp
+++ src/smooth/smooth.lisp
@@ -36,49 +36,49 @@
 (in-package :math/smooth)
 
 (defun gauss-smoothing (d)
-  "@b(Описание:) функция @b(gauss-smoothing)
+  "@b(Описание:) функция @b(gauss-smoothing) возвращает значение
+гауссовой весовой функции для расстояния @b(d), базируется на exp(-d^2).
 
  @b(Пример использования:)
 @begin[lang=lisp](code)
- (loop :for d :from 0 :to 4 :by 1/10 :collect
-     (list d (gauss-smoothing d)))
-@end(code)"  
+ (gauss-smoothing 0.0) => 1.0
+ (gauss-smoothing 1.0) => 0.36787944
+@end(code)"
   (exp (* -1 d d)))
 
 (defun exp-smoothing (d)
-  "@b(Описание:) функция @b(exp-smoothing)
+  "@b(Описание:) функция @b(exp-smoothing) возвращает экспоненциальную
+весовую функцию exp(-d).
 
  @b(Пример использования:)
 @begin[lang=lisp](code)
- (loop :for d :from 0 :to 4 :by 1/10 :collect
-     (list d (exp-smoothing d)))
-@end(code)"  
+ (exp-smoothing 0.0) => 1.0
+ (exp-smoothing 1.0) => 0.36787944
+@end(code)"
   (exp (* -1 d)))
 
 (defun cauchy-smoothing (d)
-  "@b(Описание:) функция @b(cauchy-smoothing)
+  "@b(Описание:) функция @b(cauchy-smoothing) возвращает вес по формуле
+1/(1+d^2) (Коши).
 
  @b(Пример использования:)
 @begin[lang=lisp](code)
- (loop :for d :from 0 :to 4 :by 1/10 :collect
-     (list d (cauchy-smoothing d)))
-@end(code)"  
+ (cauchy-smoothing 0.0) => 1.0
+ (cauchy-smoothing 1.0) => 0.5
+@end(code)"
   (/ 1 (+ 1 (* d d))))
 
 (defun hann-smoothing (d)
-  "@b(Описание:) функция @b(hann-smoothing)
+  "@b(Описание:) функция @b(hann-smoothing) возвращает весовую функцию
+Ханна: 0.5*(1+cos(pi*d)) для d<1 иначе 0.
 
  @b(Пример использования:)
 @begin[lang=lisp](code)
-  (loop :for d :from 0 :to 4 :by 0.1 :do
-     (format t \"~{~5F~^ ~}~%\" 
-             (list d 
-               (gauss-smoothing  d)
-               (exp-smoothing    d)
-               (cauchy-smoothing d)
-               (hann-smoothing   d))))
-@end(code)"  
-  (if (< d 1) (* 1/2 ( + 1 ( cos (* pi d)))) 0))
+ (hann-smoothing 0.0) => 1.0
+ (hann-smoothing 0.5) => 0.5
+ (hann-smoothing 1.0) => 0.0
+@end(code)"
+  (if (< d 1) (* 1/2 (+ 1 (cos (* pi d)))) 0))
 
 (defun weight-func-list ()
   (list #'gauss-smoothing
@@ -87,4 +87,12 @@
 	#'hann-smoothing))
 
 (defun weight-func-p (func)
-  (if (member func (weight-func-list)) t nil))
+  "@b(Описание:) предикат @b(weight-func-p) возвращает t, если @b(func)
+является одной из поддерживаемых весовых функций.
+
+ @b(Пример использования:)
+@begin[lang=lisp](code)
+ (weight-func-p #'gauss-smoothing) => t
+ (weight-func-p #'list) => nil
+@end(code)"
+  (not (null (member func (weight-func-list) :test #'eq))))
