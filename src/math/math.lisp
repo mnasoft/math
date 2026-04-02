afb8a7849fa307247f6cb0274e6f1fdae532be73
Mykola Matvyeyev <mnasoft@gmail.com>
2026-04-02 22:20:14 +0300

    Улучшена документация mult-matr-vect: добавлены Переменые и минималистичные примеры с результатами

--- src/math/math.lisp
+++ src/math/math.lisp
@@ -20,24 +20,29 @@
 
 (defun mult-matr-vect (matr vect)
   "@b(Описание:) функция @b(mult-matr-vect) возвращает вектор, являющийся
-результатом умножения матрицы @b(matr) на вектор @b(vect). Количество
-элементов в результирующем векторе равно количеству элементов в
-векторе @b(vect).
+результатом умножения квадратной матрицы @b(matr) на вектор @b(vect).
+Размерность результирующего вектора совпадает с размерностью @b(vect).
 
+ @b(Переменые:)
+@begin(list)
+ @item(matr - двумерный массив (матрица) размером n×n;)
+ @item(vect - одномерный массив (вектор) размером n.)
+@end(list)
 
  @b(Пример использования:)
 @begin[lang=lisp](code)
-(defparameter *m-test*
-  (make-array '(3 4)
-	      :initial-contents
-	      '((10.0d0 11.0d0  12.0d0  4.0d0)
-		(15.0d0 17.0d0  21.0d0  2.0d0)
-		(70.0 8.0  10.0 3.0))))
-(mult-matr-vect
- *m-test* ; Проверка правильности решения (системы линейных алгебраических уравнений) СЛАУ
- (solve-linear-system-rotation (cl-utilities:copy-array *m-test*)))
-@end(code)
-"
+ (mult-matr-vect
+  (make-array '(2 2) :initial-contents '((1.0d0 2.0d0) (3.0d0 4.0d0)))
+  (make-array 2 :initial-contents '(1.0d0 1.0d0)))
+ => #(3.0d0 7.0d0)
+
+ (mult-matr-vect
+  (make-array '(3 3) :initial-contents '((1.0d0 0.0d0 0.0d0)
+                                         (0.0d0 2.0d0 0.0d0)
+                                         (0.0d0 0.0d0 3.0d0)))
+  (make-array 3 :initial-contents '(2.0d0 3.0d0 4.0d0)))
+ => #(2.0d0 6.0d0 12.0d0)
+@end(code)"
   (let* ((n (array-dimension vect 0))
 	 (vect-rez (make-array n :initial-element 0.0d0)))
     (do ((i 0 (1+ i)))
